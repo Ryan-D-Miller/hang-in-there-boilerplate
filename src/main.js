a969e0e0de7c76ee7e16bb1adf0b42556126436f
Ryan Miller <ryandmiller3521@gmail.com>
2021-02-11 15:52:14 -0700

    Added saving feature for posters

--- src/main.js
+++ src/main.js
@@ -2,17 +2,21 @@
 var posterImage = document.querySelector('.poster-img');
 var posterTitle = document.querySelector('.poster-title');
 var posterQuote = document.querySelector('.poster-quote');
-var showRandomButton = document.querySelector('.show-random');
+
 var userPosterImage = document.querySelector('#poster-image-url');
 var userPosterTitle = document.querySelector('#poster-title');
 var userPosterQuote = document.querySelector('#poster-quote');
 var makePoster = document.querySelector('.make-poster');
+//buttons
+var showRandomButton = document.querySelector('.show-random');
 var showFormButton = document.querySelector('.show-form');
 var showSavedPostersButton = document.querySelector('.show-saved');
 var backToMainButton = document.querySelector('.back-to-main');
 var takeMeBackButton = document.querySelector('.show-main');
+var savePosterButton = document.querySelector('.save-poster');
+
 var savedPosters = [];
-var currentPoster;
+var currentPoster = new Poster();
 
 // event listeners go here 👇
 window.addEventListener('load', createRandomPoster);
@@ -33,8 +37,10 @@ backToMainButton.addEventListener('click', function(){
   switchScreens(".saved-posters",".main-poster");
 }, false);
 takeMeBackButton.addEventListener('click', function(){
-  switchScreens(".saved-posters",".main-poster");
+  switchScreens(".poster-form",".main-poster");
 }, false);
+
+savePosterButton.addEventListener('click', savePoster);
 
 // functions and event handlers go here 👇
 function getRandomIndex(array) {
@@ -64,12 +70,23 @@ function switchScreens(closingWindow, openingWindow){
 }
 
 function userInput() {
-  var newPoster = new Poster(userPosterImage.value, userPosterTitle.value, userPosterQuote.value);
-  posterImage.src = newPoster.imageURL;
-  posterTitle.innerHTML = newPoster.title;
-  posterQuote.innerHTML = newPoster.quote;
-  images.push(newPoster.imageURL);
-  titles.push(newPoster.title);
-  quotes.push(newPoster.quote);
-  savedPosters.push(newPoster);
+  currentPoster = new Poster(userPosterImage.value, userPosterTitle.value, userPosterQuote.value);
+  posterImage.src = currentPoster.imageURL;
+  posterTitle.innerHTML = currentPoster.title;
+  posterQuote.innerHTML = currentPoster.quote;
+  images.push(currentPoster.imageURL);
+  titles.push(currentPoster.title);
+  quotes.push(currentPoster.quote);
+}
+
+function savePoster() {
+  var duplicate = false;
+  for (var i= 0; i < savedPosters.length; i++) {
+    if (savedPosters[i] === currentPoster) {
+      duplicate = true;
+    }
+  }
+  if (!duplicate) {
+      savedPosters.push(currentPoster);
+  }
 }
