f22deb810b82c815e8894f6d20255d820f815cdb
Melanie Daoheuang <daoh0031@gmail.com>
2021-02-14 22:29:45 -0700

    Organize querySelectors and addeventListeners

--- src/main.js
+++ src/main.js
@@ -1,54 +1,49 @@
-// query selector variables go here 👇
-var posterImage = document.querySelector('.poster-img');
-var posterTitle = document.querySelector('.poster-title');
-var posterQuote = document.querySelector('.poster-quote');
-var savedPosterLocation = document.querySelector('.saved-posters-grid');
+var backToMainButton = document.querySelector('.back-to-main');
 var imageError = document.querySelector('.img-error');
-var titleError = document.querySelector('.title-error');
+var makePoster = document.querySelector('.make-poster');
 var quoteError = document.querySelector('.quote-error');
-
+var posterImage = document.querySelector('.poster-img');
+var posterQuote = document.querySelector('.poster-quote');
+var posterTitle = document.querySelector('.poster-title');
+var savePosterButton = document.querySelector('.save-poster');
+var savedPosterLocation = document.querySelector('.saved-posters-grid');
+var showFormButton = document.querySelector('.show-form');
+var showSavedPostersButton = document.querySelector('.show-saved');
+var showRandomButton = document.querySelector('.show-random');
+var takeMeBackButton = document.querySelector('.show-main');
+var titleError = document.querySelector('.title-error');
 var userPosterImage = document.querySelector('#poster-image-url');
 var userPosterTitle = document.querySelector('#poster-title');
 var userPosterQuote = document.querySelector('#poster-quote');
-var makePoster = document.querySelector('.make-poster');
-//buttons
-var showRandomButton = document.querySelector('.show-random');
-var showFormButton = document.querySelector('.show-form');
-var showSavedPostersButton = document.querySelector('.show-saved');
-var backToMainButton = document.querySelector('.back-to-main');
-var takeMeBackButton = document.querySelector('.show-main');
-var savePosterButton = document.querySelector('.save-poster');
 
-var savedPosters = [];
 var currentPoster = new Poster();
-
-// event listeners go here 👇
+var savedPosters = [];
+ 
 window.addEventListener('load', createRandomPoster);
 showRandomButton.addEventListener('click', createRandomPoster);
 makePoster.addEventListener('click', function(event) {
   userInput();
   event.preventDefault();
 });
+backToMainButton.addEventListener('click', function() {
+  switchScreens(".saved-posters",".main-poster");
+}, false);
+savePosterButton.addEventListener('click', savePoster);
 showFormButton.addEventListener('click', function() {
   switchScreens(".main-poster",".poster-form");
 }, false);
 showSavedPostersButton.addEventListener('click', function() {
   switchScreens(".main-poster",".saved-posters");
 }, false);
-backToMainButton.addEventListener('click', function() {
-  switchScreens(".saved-posters",".main-poster");
-}, false);
 takeMeBackButton.addEventListener('click', function() {
   switchScreens(".poster-form",".main-poster");
 }, false);
-savePosterButton.addEventListener('click', savePoster);
 document.addEventListener('dblclick', function(e) {
   if (e.target && e.target.parentElement.classList.contains("saved-poster")) {
     removePoster(e.target.parentElement);
   }
-})
+});
 
-// functions and event handlers go here 👇
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
